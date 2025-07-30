e5eb386632d9d5081d59c4393066a3df8786dfb8
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-07-30 15:58:02 -0600

    new files added

--- src/test/java/JavaProgramming/Strings/FirstLetterOfEachWord.java
+++ src/test/java/JavaProgramming/Strings/FirstLetterOfEachWord.java
@@ -4,7 +4,8 @@ public class FirstLetterOfEachWord {
     public static void main(String[] args) {
         String sen="My Name Is Paran Jyothi Nagarjuna Koppala With Loverly Eesha";
         String fl="";
-        for(String word:sen.split(" ")){
+        for(String word:sen.split(" ")) //split the word before getting the 1st letter of each word
+        {
             fl=fl+word.charAt(0);
         }
         System.out.println(fl);

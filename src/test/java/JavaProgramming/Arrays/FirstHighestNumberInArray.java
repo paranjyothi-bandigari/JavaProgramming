fbf1c237fc7e3e4a36b54d22cd85c8246ffda178
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-08-24 16:31:55 -0600

    new files added

--- src/test/java/JavaProgramming/Arrays/FirstHighestNumberInArray.java
+++ src/test/java/JavaProgramming/Arrays/FirstHighestNumberInArray.java
@@ -1,7 +1,7 @@
 package JavaProgramming.Arrays;
 
 import java.util.Arrays;
-//1st Highest number in array
+//1st Highest number in an array
 public class FirstHighestNumberInArray {
     public static void main(String[] args) {
         int[] num ={5,4,7,8,9,2,1}; //1 2 4 5 7 8 9

04ce433525a0fb5aaa9d1456a6cb8cff9e810d71
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-15 16:23:27 -0600

    new files added

---
+++ src/test/java/questions.java
@@ -0,0 +1,38 @@
+public class questions {
+//    Java program to remove duplicates characters from given String.
+//    👉Program Remove the second highest element from the HashMap.
+//    👉Java program to Generate prime numbers between 1 & given 4 number
+//    👉How to find the missing values from a sorted array.
+//            👉Java program to input name, middle name and surname of a person and print only the initials.
+//            👉Program to Print all Treemap elements?
+//            👉What is a singleton Design Pattern? How do you implement that in your framework?
+//            👉Write the Top 5 test cases for Booking Coupons.
+//
+//            👉What is serialization and deserialization?
+//            👉What is the Difference between status codes 401 and 402?
+//            👉Difference between selenium 3 and selenium 4?
+//            👉What is delegate in Java and where do you use Delegate in your Framework?
+//            👉How many maximum thread-pool can you open in the TestNG?
+//            👉What are the Major challenges that come into the picture when you do parallel testing using TestNG and Grid?
+//            👉How do you integrate your automation framework with the Jenkins pipeline?
+//            👉What will happen if we remove the main method from the java program?
+//            👉What is the component of your current Project?
+//            👉How do you pass parameters in TestNG?
+//            👉Write the logic of retrying the failed test case with a minimum 3 numbers of time in
+//    Automation Testing. Which Interface do you use for it?
+//            👉What is the OOPs concept in java?
+//            👉Difference Between Classes and Objects?
+//            👉What is collection in Java?
+//            👉In How many ways can we create an object?
+//            👉Why is Java not 100% Object-oriented?
+//            👉Can we make a constructor as Static?
+//            👉How to convert a JSON to java object using Jackson? POJO
+//👉What is the difference between Abstraction Class and Interfaces?
+//            👉Difference between String, StringBuilder, and Stringbuffer?
+//            👉What are other immutable classes in Java apart from String?
+//            👉Difference between TreeMap and HashMap?
+//            👉How do you set priorities for test automation, which test needs to be automated first?
+//            👉How do you set test case priorities for your team?
+//            👉What are the functional things you need to test on e-commerce sites?
+
+}

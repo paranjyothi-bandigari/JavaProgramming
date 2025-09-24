398127cd40f30195296233d861cbf9bf91da0750
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-24 15:45:55 -0600

    new files added

--- src/test/java/JavaProgramming/Lists/revList.java
+++ src/test/java/JavaProgramming/Lists/revList.java
@@ -1,4 +1,14 @@
 package JavaProgramming.Lists;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class revList {
+    public static void main(String[] args) {
+
+        List<Integer> intList1 = Arrays.asList(11, 32, 35, 41, 59, 65, 77, 89);
+        for(int i=intList1.size()-1;i>=0;i--){
+            System.out.println(intList1.get(i));
+        }
+    }
 }

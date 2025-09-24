dca8184e84250089d9772aed18adfabcbf0e990a
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-24 14:55:49 -0600

    new files added

--- src/test/java/JavaProgramming/Lists/IntList.java
+++ src/test/java/JavaProgramming/Lists/IntList.java
@@ -1,12 +1,16 @@
 package JavaProgramming.Lists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IntList {
     public static void main(String[] args) {
 
         List<Integer> intList=new ArrayList<>();
+       // List<Integer> intList2= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+
         intList.add(2);
         intList.add(5);
         intList.add(7);

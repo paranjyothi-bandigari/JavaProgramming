2ce27f9c21bb46404aa8995dc03309045d4cdfc2
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-24 13:05:27 -0600

    new files added

--- src/test/java/JavaProgramming/Lists/IntList.java
+++ src/test/java/JavaProgramming/Lists/IntList.java
@@ -7,6 +7,26 @@ public class IntList {
     public static void main(String[] args) {
 
         List<Integer> intList=new ArrayList<>();
+        intList.add(2);
+        intList.add(5);
+        intList.add(7);
+        intList.add(1);
+        intList.add(8);
+        intList.add(6);
+        intList.add(9);
+        for(Integer num:intList){
+            if(num>2 && num%2==0){
+               System.out.println(num +"Is greater than 2 and even");
+            }else
+                {
+                System.out.println(num+"is either equal to 2 or odd");
+            }
+        }
+
+
+
+
+
 
 
     }

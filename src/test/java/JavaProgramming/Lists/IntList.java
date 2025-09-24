917a522ea3f52b3688123f950599a323d9e3df3f
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-24 13:00:04 -0600

    new files added

---
+++ src/test/java/JavaProgramming/Lists/IntList.java
@@ -0,0 +1,13 @@
+package JavaProgramming.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntList {
+    public static void main(String[] args) {
+
+        List<Integer> intList=new ArrayList<>();
+
+
+    }
+}

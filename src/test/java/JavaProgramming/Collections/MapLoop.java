9a74f7bdf8d3a38315417602d7a3fd721d57181d
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-25 12:59:41 -0600

    new files added

--- src/test/java/JavaProgramming/Collections/MapLoop.java
+++ src/test/java/JavaProgramming/Collections/MapLoop.java
@@ -5,7 +5,12 @@ import java.util.HashMap;
 public class MapLoop {
     public static void main(String[] args) {
         Map<String, Integer> studentScore=new HashMap<>();
+        studentScore.put("John", 85);
+        studentScore.put("Emily", 92);
+        studentScore.put("Michael", 78);
+        studentScore.put("Jessica", 95);
 
+        System.out.println(studentScore);
 
     }
 }

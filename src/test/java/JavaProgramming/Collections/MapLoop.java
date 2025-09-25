e8a7e41a0386b981b41258684604f5ad2e4a0324
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-25 12:58:45 -0600

    new files added

---
+++ src/test/java/JavaProgramming/Collections/MapLoop.java
@@ -0,0 +1,11 @@
+package JavaProgramming.Collections;
+import java.util.Map;
+import java.util.HashMap;
+
+public class MapLoop {
+    public static void main(String[] args) {
+        Map<String, Integer> studentScore=new HashMap<>();
+
+
+    }
+}

e8c5563897fdac78889f9f65f03da759c067a4a3
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-25 13:44:47 -0600

    new files added

--- src/test/java/JavaProgramming/Collections/ValueSet.java
+++ src/test/java/JavaProgramming/Collections/ValueSet.java
@@ -1,4 +1,21 @@
 package JavaProgramming.Collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ValueSet {
+    public static void main(String[] args) {
+        Map<String, Integer> studentScore = new HashMap<>();
+        studentScore.put("John", 85);
+        studentScore.put("Emily", 92);
+        studentScore.put("Michael", 78);
+        studentScore.put("Jessica", 95);
+        System.out.println(studentScore);
+
+        for(Integer values:studentScore.values()){
+            System.out.println(values);
+        }
+
+    }
 }
+

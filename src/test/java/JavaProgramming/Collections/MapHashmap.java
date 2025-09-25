9ddb13f5aaa9b218e8d2a31298a60b54faf40005
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-25 13:30:33 -0600

    new files added

--- src/test/java/JavaProgramming/Collections/MapHashmap.java
+++ src/test/java/JavaProgramming/Collections/MapHashmap.java
@@ -1,23 +1,22 @@
 package JavaProgramming.Collections;
-import java.util.Map;
-import java.util.HashMap;
 
-public class MapLoop {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapHashmap {
     public static void main(String[] args) {
         Map<String, Integer> studentScore=new HashMap<>();
         studentScore.put("John", 85);
         studentScore.put("Emily", 92);
         studentScore.put("Michael", 78);
         studentScore.put("Jessica", 95);
-
         System.out.println(studentScore);
 
         for(Map.Entry<String, Integer> entry:studentScore.entrySet()){
-            String keys=entry.getKey();
-            Integer values=entry.getValue();
-            System.out.println(keys);
-            System.out.println(values);
-        }
 
+            if((entry.getKey().contains("John"))||(entry.getKey().contains("Emily"))){
+                System.out.println(entry.getKey()+" :" +entry.getValue());
+            }
+        }
     }
 }

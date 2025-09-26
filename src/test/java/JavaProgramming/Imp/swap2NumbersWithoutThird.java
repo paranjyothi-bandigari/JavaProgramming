b6f1f82912f1f8d77cd9c0b7404336891ea616b9
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-26 14:52:57 -0600

    new files added

---
+++ src/test/java/JavaProgramming/Imp/swap2NumbersWithoutThird.java
@@ -0,0 +1,15 @@
+package JavaProgramming.Imp;
+
+public class swap2NumbersWithoutThird {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+
+         a=a+b; //a=2+3=5
+         b=a-b; //b=5-3=2
+         a=a-b; //a=5-2=3
+        System.out.println(a+" is a");
+        System.out.println(b+" is b");
+
+    }
+}

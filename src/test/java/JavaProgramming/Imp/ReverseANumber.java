1b53d8b94dc1beefc15b8e2d4cb4f097b2d6060f
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-09-26 15:38:15 -0600

    new files added

---
+++ src/test/java/JavaProgramming/Imp/ReverseANumber.java
@@ -0,0 +1,10 @@
+package JavaProgramming.Imp;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+        int num=987654321;
+        int rev;
+        
+
+    }
+}
---
+++ src/test/java/JavaProgramming/Imp/ReverseString.java
@@ -0,0 +1,14 @@
+package JavaProgramming.Imp;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String name="Paranjyothi";
+        String rev="";
+
+        for(int i=name.length()-1;i>=0;i--){
+            rev=rev+name.charAt(i);
+
+        }
+        System.out.println(rev);
+    }
+}

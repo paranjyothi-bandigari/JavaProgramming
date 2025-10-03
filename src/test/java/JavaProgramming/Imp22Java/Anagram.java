0867c7fbc876b42e0b2c91730e4307a29fb191b8
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-03 16:58:51 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/Anagram.java
+++ src/test/java/JavaProgramming/Imp22Java/Anagram.java
@@ -1,4 +1,30 @@
 package JavaProgramming.Imp22Java;
 
+import java.util.Arrays;
+
+/* function to check whether two strings are
+	    anagram of each other */
+//To determine if two strings are anagrams, they must contain
+// the same characters with the same frequencies, regardless
+// of the order of characters.
 public class Anagram {
+    public static void main(String[] args) {
+        String s1="Paranjyothi";
+        String s2="paranjyoith";
+        s1.toLowerCase();
+        s2.toLowerCase();
+        if(s1.length()==s2.length()) {
+            char[] c1 = s1.toCharArray();
+            char[] c2 = s2.toCharArray();
+            Arrays.sort(c1);
+            Arrays.sort(c2);
+            if(c1.equals(c2)){}
+            System.out.println("Anagram");
+        }
+        else {
+            System.out.println("Not Anagram");
+
+        }
+    }
+
 }

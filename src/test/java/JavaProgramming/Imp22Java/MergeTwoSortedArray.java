c6c9550825a44cd7154747e2d947727956cc17e9
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-07 17:15:27 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/MergeTwoSortedArray.java
+++ src/test/java/JavaProgramming/Imp22Java/MergeTwoSortedArray.java
@@ -1,13 +1,22 @@
 package JavaProgramming.Imp22Java;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MergeTwoSortedArray {
 
     public static void main(String[] args) {
         int[] a={2,3,5};
         int[] b={6,1,8};
-        int m=a.length;
-        int n=b.length;
-        int[] c=new int[m+n];
-
-    }
-}
+        Arrays.sort(a);
+        Arrays.sort(b);
+        ArrayList list=new ArrayList();
+        for(int n:a){
+            list.add(n);
+        }
+        for(int m:b) {
+            list.add(m);
+        }
+        System.out.println(list);
+}}

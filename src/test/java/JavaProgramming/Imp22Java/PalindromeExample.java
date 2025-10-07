3a7ba006115f7c14f4b3e943e02371abc23fb22f
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-07 17:35:12 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/PalindromeExample.java
+++ src/test/java/JavaProgramming/Imp22Java/PalindromeExample.java
@@ -1,4 +1,24 @@
 package JavaProgramming.Imp22Java;
 
 public class PalindromeExample {
+    public static void main(String[] args) {
+        String name="madam";
+        String rev=new StringBuilder(name).reverse().toString();
+        if(name.equals(rev)){
+        System.out.println("Palandrome");}
+        else System.out.println("Not Palandrome");
+
+        int num=434;
+        int OriginalNum=num;
+        int numrev=0;
+        while (num>0){
+            numrev=numrev*10+num%10;
+            num=num/10;
+        }
+        if(OriginalNum==numrev){
+            System.out.println("palindrome");
+        }
+        else
+            System.out.println("not Palindrome");
+    }
 }

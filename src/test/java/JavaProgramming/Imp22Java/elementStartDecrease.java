ba8551ac891fdf36074ffc786e3c97921fc8ffd0
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-07 14:50:13 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/elementStartDecrease.java
+++ src/test/java/JavaProgramming/Imp22Java/elementStartDecrease.java
@@ -1,4 +1,15 @@
 package JavaProgramming.Imp22Java;
-
+//The code tries to find the index where the array stops increasing and starts decreasing — basically the peak point.
 public class elementStartDecrease {
+    public static void main(String[] args) {
+        int[] arrnum={0,1,2,3,2,1};
+        int midnum=0;
+        for(int i=0;i<arrnum.length-1;i++){
+            if(arrnum[i]>arrnum[i+1]){
+                midnum=i;
+                break;
+            }
+        }
+        System.out.println(midnum);
+    }
 }

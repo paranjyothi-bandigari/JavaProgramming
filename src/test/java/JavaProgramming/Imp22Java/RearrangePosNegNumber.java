25d31f168a69c9ce55e98272405ceb1c109663e6
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-07 17:59:47 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/RearrangePosNegNumber.java
+++ src/test/java/JavaProgramming/Imp22Java/RearrangePosNegNumber.java
@@ -1,4 +1,12 @@
 package JavaProgramming.Imp22Java;
 
+import java.util.Arrays;
+
 public class RearrangePosNegNumber {
+    public static void main(String[] args) {
+        int[] arr = {-1, 2, -3, 4, 5, 6, -7, 8, 9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
 }

961d52d0b430abf7b5b49932acd50798bcfc6e6c
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-07 14:41:56 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/CommonInTwoArrays.java
+++ src/test/java/JavaProgramming/Imp22Java/CommonInTwoArrays.java
@@ -1,7 +1,22 @@
 package JavaProgramming.Imp22Java;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class CommonInTwoArrays {
     public static void main(String[] args) {
-
+        Integer a[] = { 1, 2, 6, 3, 4, 5 };
+        Integer b[] = { 2, 4, 3, 1, 0 };
+        Set<Integer> common=new HashSet<>();
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<b.length;j++){
+                if(a[i]==b[j]){
+                    common.add(a[i]);
+                    break;
+                }
+            }
+        }
+        System.out.println(common);
     }
 }

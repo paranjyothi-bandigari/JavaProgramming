213204ee70fc1a9dd9580a129d07afc382fc35ba
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-09 15:09:08 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/SumUntilSingleDigit.java
+++ src/test/java/JavaProgramming/Imp22Java/SumUntilSingleDigit.java
@@ -1,4 +1,20 @@
 package JavaProgramming.Imp22Java;
 
 public class SumUntilSingleDigit {
+    public static void main(String[] args) {
+        //int num=2345;
+       int ans= addDigits(2345);
+        System.out.println(ans);
+    }
+    public static int addDigits(int num){
+        if(num<10){
+        return num;}
+
+        int sum=0;
+        while(num>0){
+            sum+=num%10; //0+2345%10=
+            num/=10; // 2345/10
+        }
+        return addDigits(sum);
+    }
 }
--- src/test/java/JavaProgramming/Imp22Java/segreggatearray.java
+++ src/test/java/JavaProgramming/Imp22Java/segreggatearray.java
@@ -1,4 +1,22 @@
 package JavaProgramming.Imp22Java;
 
+import java.util.LinkedList;
+
 public class segreggatearray {
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{ 0, 1, 0, 1, 1, 1 };
+
+        LinkedList li=new LinkedList();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                li.addFirst(arr[i]);
+            }
+            else{
+                li.addLast(arr[i]);
+            }
+        }
+        System.out.println(li);
+
+    }
 }

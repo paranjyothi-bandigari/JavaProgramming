e961f259c307b05951b580868f72ee96b16096da
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-09 14:47:26 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/RevWordInSentence.java
+++ src/test/java/JavaProgramming/Imp22Java/RevWordInSentence.java
@@ -1,4 +1,14 @@
 package JavaProgramming.Imp22Java;
 
 public class RevWordInSentence {
+    public static void main(String[] args) {
+        String s = "i like this program very much";
+        String newS[]=s.split(" ");
+        String rev="";
+        for(int i=newS.length-1;i>=0;i--){
+            rev+=newS[i]+" ";
+        }
+        System.out.println(rev);
+    }
 }
+
--- src/test/java/JavaProgramming/Imp22Java/pairElements.java
+++ src/test/java/JavaProgramming/Imp22Java/pairElements.java
@@ -1,4 +1,21 @@
 package JavaProgramming.Imp22Java;
 
 public class pairElements {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, -1, 5};
+        int targetSum = 6;
+        findAndPrintPair(arr,targetSum);
+    }
+
+    private static void findAndPrintPair(int[] arr, int targetSum) {
+        System.out.println("Pairs with sum " + targetSum + ":");
+        for(int i=0;i<arr.length;i++){
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]==targetSum){
+                System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+            }
+        }
+    }
+
+    }
 }

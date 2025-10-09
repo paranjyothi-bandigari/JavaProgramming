ac78e1f7a81afc23e417b4eb5d2253ab4c200021
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-09 13:15:16 -0600

    new files added

--- src/test/java/JavaProgramming/Imp22Java/CommonInTwoArrays.java
+++ src/test/java/JavaProgramming/Imp22Java/CommonInTwoArrays.java
@@ -6,8 +6,8 @@ import java.util.Set;
 
 public class CommonInTwoArrays {
     public static void main(String[] args) {
-        Integer a[] = { 1, 2, 6, 3, 4, 5 };
-        Integer b[] = { 2, 4, 3, 1, 0 };
+        int a[] = { 1, 2, 6, 3, 4, 5 };
+        int b[] = { 2, 4, 3, 1, 0 };
         Set<Integer> common=new HashSet<>();
         for(int i=0;i<a.length;i++){
             for(int j=0;j<b.length;j++){

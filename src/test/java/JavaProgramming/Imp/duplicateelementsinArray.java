3129afb4c4a4fab5912847d5da5d114087955a19
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-10 13:22:04 -0600

    new files added

---
+++ src/test/java/JavaProgramming/Imp/duplicateelementsinArray.java
@@ -0,0 +1,19 @@
+package JavaProgramming.Imp;
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class duplicateelementsinArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            if (set.contains(array[i])) {
+                System.out.println("Duplicate element found: " + array[i]);
+            } else {
+                set.add(array[i]);
+            }
+        }
+    }
+
+}

7d130e3c9c653fbdcfab2488f27262afb2c93ae3
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-27 12:45:29 -0600

    new files added

--- src/test/java/Java_Practise/testPractise.java
+++ src/test/java/Java_Practise/testPractise.java
@@ -1,4 +1,21 @@
 package Java_Practise;
+//1. Write a Java method to check if a given substring exists within a string. string "Hello world" substring="Hello".
+//2. Write a Java method to count the number of times a substring occurs in a given string
+//3. Extracting a Subdomain from a URL: String url = "https://subdomain.example.com";
+//4. Extracting a File Extension:String filename = "document.pdf";
+//5. Reverse A string in 5 ways. a.Using StringBuilder or StringBuffer b.Using a for Loop c.Using Character Array d.Using Java 8 Streams e.Using Recursion
+//6.check if a String is a palindrome?
+//7. remove whitespace from the beginning and end of a String?
+//8.  Explain how you would handle string comparisons in test automation assertions.
+//Answer: Use the assertEquals method from a testing framework like JUnit or TestNG to compare expected and actual string values.
+//9.: Describe a scenario where string manipulation might be necessary in test automation.
+//Answer: String manipulation is necessary when dealing with dynamic data. For example, extracting specific parts of a URL to verify its correctness or formatting dates and times for input fields.
+//10.ReverseWordsInSentence
+//11.String Interning:
+//Java uses a unique feature called "string interning" to optimize memory usage. When you create a string literal, the JVM checks if the string already exists in the string pool. If it does, the new reference points to the existing string. This reduces memory overhead for frequently used strings.
+//12.String's Performance Impact with Concatenation
+//Concatenating strings using the + operator inside a loop can lead to performance issues because each concatenation creates a new string object. Instead, using StringBuilder or StringBuffer is recommended for efficient string manipulation.
+
 
 public class testPractise {
     public static void main(String[] args) {
@@ -7,5 +24,7 @@ public class testPractise {
 
     test objAdd=new test();
     objAdd.add(1,8);
+
+
     }
 }

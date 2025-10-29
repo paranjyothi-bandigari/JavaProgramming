eb45a06bc7d5626415625e931eba1774307735c4
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-10-29 13:43:34 -0600

    new files added

--- src/test/java/Java_Practise/Strings/subString.java
+++ src/test/java/Java_Practise/Strings/subString.java
@@ -1,8 +1,7 @@
 package Java_Practise.Strings;
 
-
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 
 public class subString {
@@ -13,6 +12,7 @@ public class subString {
        String str = "Hello world";
        String substr = "world";
        System.out.println(str.contains(substr));
+
    }
 //2. Write a Java method to count the number of times a substring occurs in a given string
 @Test()
@@ -21,51 +21,45 @@ public void subStringOccurance(){
         String sub = "World";
         int count=0;
         String[] st=s.split(" ");
-        for(int i=0;i<st.length;i++){
-            if(st[i].contains(sub)){
+        for(String word:st){
+            if(word.contains(sub)){
                 count++;
             }
         }
-        System.out.println(count);}
+    System.out.println("SubString Word is : "+count);
+       }
     @Test()
     public void subdomain() {
-//3. Extracting a Subdomain from a URL: String url = "https://subdomain.example.com";
+//3. Extracting a 'Subdomain' from a URL: String url = "https://subdomain.example.com";
         String url = "https://subdomain.example.com";
-        int start = url.indexOf("://")+3;
-        int end = url.indexOf(".");
-        System.out.println(start);
-        System.out.println(end);
-        System.out.println(url.substring(start,end));
-
+        int startsubdomain=url.indexOf("://")+3;
+        int endsubdomain=url.indexOf(".",startsubdomain);
+        System.out.println(url.substring(startsubdomain,endsubdomain));
 //4. Extracting a File Extension:String filename = "document.pdf";
         String filename = "document.pdf";
-        int endd = filename.lastIndexOf(".")+1;
-        System.out.println(endd);
-        System.out.println(filename.substring(endd));
-
+        int end=filename.lastIndexOf(".")+1;
+        System.out.println(filename.substring(end));
     }
     @Test()
     public void revString(){
 //5. Reverse A string in 5 ways.
 // a.Using StringBuilder or StringBuffer
-            String name = "java";
-            StringBuilder sb = new StringBuilder(name);
-            String reversedString=sb.reverse().toString();
-            System.out.println(reversedString);
+        String name = "java";
+        StringBuilder sb=new StringBuilder(name);
+        System.out.println(sb.reverse());
 // b.Using a for Loop
-            String rev = "";
-            //StringBuilder sb=new StringBuilder();
-            for (int i = name.length()-1; i>=0; i--) {
-                rev += name.charAt(i);
-            }
-            System.out.println(rev);
+        String revName="";
+        for(int i=name.length()-1;i>=0;i--){
+        revName=revName+name.charAt(i);
+}
+        System.out.println(revName);
 // c.Using Character Array
-            String rev1 = "";
-            char[] name1 = name.toCharArray();
-            for (int i = name.length()-1; i>=0; i--) {
-                rev1 += name.charAt(i);
-            }
-        System.out.println(rev1);
+        String revName1="";
+        char[] revN=name.toCharArray();
+        for(int i=revN.length-1;i>=0;i--) {
+            revName1 = revName1 + name.charAt(i);
+        }
+        System.out.println(revName1);
 // d.Using Java 8 Streams
 // e.Using Recursion
         }
@@ -73,50 +67,49 @@ public void subStringOccurance(){
     public void palandrome() {
             //6.check if a String is a palindrome?
             String p1 = "level";
-            StringBuilder sb = new StringBuilder(p1).reverse();
-            String p2 = String.valueOf(sb);
-            System.out.println(p1 + " : " + p2);
-            int l1 = p1.length();
-            int l2 = p2.length();
-            if (l1 == l2) {
-                if (p1.equals(p2)) {
-                    System.out.println("Palandrome");
-                } else
-                    System.out.println("Not Palandrome");
-            } else
-                System.out.println("l1 is not equal to l2");
+            StringBuilder sb=new StringBuilder(p1);
+            String p2=sb.reverse().toString();
+            int p1Size=p1.length();
+            int p2Size=sb.length();
+            if(p1Size==p2Size) {
+                for (int i = 0; i < p1Size; i++) {
+                    for (int j = 0; j < p2Size; j++) {
+                        if (p1.charAt(i) == p2.charAt(j)) {
+                        continue;
+                        }
+                        else
+                            System.out.println("not palandrome");
+                    }
+                }
+                System.out.println("Palandrome");
+            }
         }
     @Test()
-    public void removeWhiteSpaces(){
+    public void removeWhiteSpaces() {
 //7. remove whitespace from the beginning and end of a String?
-    String s3="  Hellowen  ";
-    String s4=s3.trim();
-        System.out.println(s4);}
+        String s3 = "  Hellowen  ";
+        String s4=s3.trim();
+        System.out.println("Trimed s3 is : "+s4);
+    }
     @Test()
-    public void assertthat(){
+    public void assertthat() {
         //Explain how you would handle string comparisons in test automation assertions.
         //Answer: Use the assertEquals method from a testing framework like JUnit or
         // TestNG to compare expected and actual string values.
-        String s5="test";
-        String s6="test";
-         assertEquals(s5,s6);}
+        String s1="Java";
+        String s2="Java";
+        Assert.assertEquals(s1,s2);
+        System.out.println("******");
+
+    }
          //9.: Describe a scenario where string manipulation might be necessary in test automation.
         //Answer: String manipulation is necessary when dealing with dynamic data. For example,
         // extracting specific parts of a URL to verify its correctness or formatting dates and times for input fields.
          @Test()
          public void ReverseWordSent() {
             //10.ReverseWordsInSentence
-
             String sw = "my name is java";
-            String wordRev;
-            String[] word = sw.split(" ");
-            StringBuilder revsent = new StringBuilder();
-            for (String words : word) {
-                wordRev = new StringBuilder(words).toString();
-                revsent.append(wordRev).append(" ");
 
-            }
-            System.out.println(revsent.toString().trim());
         }
 //11.String Interning:
 //Java uses a unique feature called "string interning" to optimize memory usage.

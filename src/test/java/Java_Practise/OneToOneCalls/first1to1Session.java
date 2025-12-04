956ef5c560832d39243a9ea0358067de3c674def
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-12-04 14:30:06 -0700

    new files added

--- src/test/java/Java_Practise/OneToOneCalls/first1to1Session.java
+++ src/test/java/Java_Practise/OneToOneCalls/first1to1Session.java
@@ -1,5 +1,8 @@
 package Java_Practise.OneToOneCalls;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class first1to1Session {
     public static void main(String[] args) {
         String input="Testing"; // replace T ,t with o
@@ -21,13 +24,16 @@ public class first1to1Session {
         }
         String[] fruit = {"mango", "Apple", "orange"};//print a friuit that starts with aeiou and print count
         int count=0;
+        List<String> l=new ArrayList<>();
         for(String f:fruit){
             if(f.matches("^[AEIOUaeiou].*")){
+                l.add(f);
                 count++;
-                System.out.println(f);
+                //System.out.println(f);
             }
         }
         System.out.println(count);
+        System.out.println(l);
         String[] newinput=input.split("t");
      for(int i=0;i< newinput.length;i++){
          System.out.println(newinput[i]);

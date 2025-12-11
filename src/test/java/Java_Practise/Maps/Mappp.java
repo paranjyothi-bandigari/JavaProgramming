d741c92725b84f571ea599f7969a6f931ae1c83e
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-12-11 12:24:14 -0700

    new files added

--- src/test/java/Java_Practise/Maps/Mappp.java
+++ src/test/java/Java_Practise/Maps/Mappp.java
@@ -1,5 +1,7 @@
 package Java_Practise.Maps;
 
+import org.apache.commons.compress.utils.OsgiUtils;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,8 +20,16 @@ public class Mappp {
         //         // Explain about looping in Map with EntrySet
         //Write a program to count the frequency of characters in a string using HashMap.
         String str = "automation";
+        Map<Character,Integer> strmap=new HashMap<>();
+        for(char c:str.toCharArray()){
+            strmap.put(c,strmap.getOrDefault(c,0)+1);
+        }
+        System.out.println(strmap);
 
         //How would you sort a HashMap by its values?
+        Map<String, Integer> maa=new HashMap<>();
+        maa.put("apple",1);
+        maa.put("banana",2);
 
         // Find missing numbers in below given array :
         //arr[]={1,3,5,7,9,8,4}; given size=9; o/p should be = 2,6
@@ -27,6 +37,14 @@ public class Mappp {
 
         // Java Program to Find Occurrences of Elements in an Array
         int[] numbers = {1, 2, 2, 3, 4, 4, 4, 5};
+        Map<Integer,Integer> numberMap=new HashMap<>();
+        for(int num:numbers){
+            if(numberMap.containsKey(num)){
+                numberMap.put(num,numberMap.get(num)+1);
+            }else
+                numberMap.put(num,1);
+        }
+        System.out.println(numberMap);
 
         // How would you merge two HashMaps in Java
         Map<String, Integer> m1=new HashMap<>();
@@ -35,6 +53,8 @@ public class Mappp {
         Map<String, Integer> m2=new HashMap<>();
         m2.put("apple",1);
         m2.put("mango",3);
+        m1.putAll(m2);
+        System.out.println(m1);
 
         // Problem: Find the Longest Substring Without Repeating Characters
         //Example: Input: s = "abcabcbb"  Output: 3 Explanation: The answer is "abc", with a length of 3
@@ -46,12 +66,34 @@ public class Mappp {
         m3.put("apple",1);
         m3.put("banana",2);
         m3.put("mango",3);
+        for(Map.Entry<String,Integer> entry : m3.entrySet()){
+            if(entry.getKey()=="banana"){
+              entry.setValue(5);
+            }
+        }
+        System.out.println(m3+"Updated Banana value to 5");
 
         // Write a Java program that counts the occurrences of each word in a given sentence using a HashMap
         String Sent="java is fun and java is powerful";
+        String[] sent=Sent.split(" ");
+        Map<String,Integer> mapSent=new HashMap<>();
+        for(String strSent:sent){
+            mapSent.put(strSent,mapSent.getOrDefault(strSent,0)+1);
+        }
+        for(Map.Entry<String,Integer> entry:mapSent.entrySet()){
+            System.out.println(entry.getKey()+" ; "+entry.getValue());
+        }
+        System.out.println(mapSent);
 
 
         String strCap="Hello there, I am Swetha and I work in Automation"; //Hello I Swetha I Automation
+        String[] strcap=strCap.split(" ");
+        Pattern p=Pattern.compile("^[A-Z][a-z]*");
+        for(String s:strcap){
+            if(p.matcher(s).find()){
+                System.out.println(s);
+            }
+        }
 
 
         String nameWithDOB="John was born on 12-05-1990 and marry on 23/11/1983"; //patternmatch -[12-05-1990, 23/11/1983]

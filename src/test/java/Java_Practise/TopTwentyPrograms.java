e21af8e904c951d080c34e6158322901ff12266b
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-12-12 21:43:55 -0700

    new files added

---
+++ src/test/java/Java_Practise/TopTwentyPrograms.java
@@ -0,0 +1,139 @@
+package Java_Practise;
+
+import org.testng.annotations.Test;
+
+public class TopTwentyPrograms {
+   @Test
+    public void anagramProgram(){
+
+
+   }
+
+@Test
+public void commonIntwoArrays(){
+
+
+}
+
+@Test
+public void countPair(){
+
+
+}
+
+@Test
+public void countVowels(){
+
+
+}
+
+@Test
+public void elementStartDecrease(){
+
+
+}
+
+@Test
+public void expressionEvaluationRED(){
+
+
+}
+
+@Test
+public void firstandLastOccurance(){
+
+
+}
+
+@Test
+public void firstRepeatingElementInArray(){
+
+
+}
+
+@Test
+public void firststrSubsequenceOfsecond(){
+
+
+}
+
+@Test
+public void maxProductArrayOfposnegInteger(){
+
+
+}
+
+@Test
+public void mergeTwoSortedArray(){
+
+
+}
+
+@Test
+public void pairElements(){
+
+
+}
+
+@Test
+public void palindromeExample(){
+
+
+}
+
+@Test
+public void rearrangePosNegNumber(){
+
+
+}
+
+@Test
+public void removeConsecutiveDuplicate(){
+
+
+}
+
+@Test
+public void revWordInSentence(){
+
+
+}
+
+@Test
+public void rotatebyN(){
+
+
+}
+
+@Test
+public void secondlargestinArray(){
+
+
+}
+
+@Test
+public void secondSmallestElementInArray(){
+
+
+}
+
+@Test
+public void secondSmallestElementInArrayWithoutSorting(){
+
+
+}
+
+@Test
+public void segreggatearray(){
+
+
+}
+
+@Test
+public void sumUntilSingleDigit(){
+
+
+}
+
+
+}

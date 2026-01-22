b706a4853242ecd9aecf039f1552c0dff6542508
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2026-01-22 16:29:59 -0700

    new files added

--- src/test/java/Java_Practise/tutu.java
+++ src/test/java/Java_Practise/tutu.java
@@ -13,11 +13,20 @@ import java.util.regex.Pattern;
 public class tutu {
         @Test
         public void anagramProgram() {
-//listen ↔ silent
+//listen ↔ silent -- same letters different order and same length
             String anagramString1 = "listen";
             String anagramString2 = "silent";
-
-
+            char[] sortedString1=anagramString1.toCharArray();
+            char[] sortedString2=anagramString2.toCharArray();
+            Arrays.sort(sortedString1);
+            Arrays.sort(sortedString2);
+                System.out.println(sortedString1);
+                System.out.println(sortedString2);
+            if(anagramString1.length()==anagramString2.length()){
+                if(Arrays.equals(sortedString1,sortedString2)){
+                        System.out.println("anagram");
+                }else System.out.println("not anagram");
+            }
         }
 
         @Test
@@ -30,7 +39,11 @@ public class tutu {
         @Test
         public void countPair() {
             //For an array of length n,
-            //👉 Number of pairs = n × (n - 1) / 2
+            // Number of pairs = n × (n - 1) / 2
+                int[] arr={1,2,3,4,5,6};
+                int n=arr.length;
+                int pairs=n*(n-1)/2;
+                System.out.println(pairs);
 
         }
 
@@ -86,7 +99,18 @@ public class tutu {
 
         @Test
         public void palindromeExample() { // rev of wor is same as given word then its palandrome
-
+                String palandromeString = "A man a plan a canal Panama";
+                String PString=palandromeString.toLowerCase();
+                String rev="";
+                PString=PString.replaceAll(" ","");
+                for(int i=PString.length()-1;i>=0;i--){
+                        rev=rev+PString.charAt(i);
+                }
+                System.out.println(rev);
+                if(PString.equals(rev)){
+                        System.out.println("Palandrome");
+                }else
+                        System.out.println("Not Palandrome");
         }
 
         @Test

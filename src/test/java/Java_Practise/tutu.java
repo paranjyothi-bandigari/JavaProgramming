38b3224202558a1af7414bc0347ca59916c14f07
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2026-01-22 15:58:35 -0700

    new files added

---
+++ src/test/java/Java_Practise/tutu.java
@@ -0,0 +1,142 @@
+package Java_Practise;
+
+import org.testng.annotations.Test;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.testng.annotations.Test;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class tutu {
+        @Test
+        public void anagramProgram() {
+//listen ↔ silent
+            String anagramString1 = "listen";
+            String anagramString2 = "silent";
+
+
+        }
+
+        @Test
+        public void commonIntwoArrays() {
+            int[] arr1 = {1, 2, 3, 4, 7};
+            int[] arr2 = {3, 5, 7, 9};
+
+        }
+
+        @Test
+        public void countPair() {
+            //For an array of length n,
+            //👉 Number of pairs = n × (n - 1) / 2
+
+        }
+
+        @Test
+        public void countVowels() {
+
+        }
+
+        @Test
+        public void elementStartDecrease() {
+
+        }
+
+        @Test
+        public void expressionEvaluationRED() {
+
+
+        }
+
+        @Test
+        public void firstandLastOccurance() {
+
+
+        }
+
+        @Test
+        public void firstRepeatingElementInArray() {
+
+
+        }
+
+        //@Test
+       // public void firststrSubsequenceOfsecond() {
+        //}
+
+        //private boolean isSubsequenceIgnoreCase(String a, String b) {
+
+        //}
+
+        @Test
+        public void maxProductArrayOfposnegInteger() {
+          }
+
+        @Test
+        public void mergeTwoSortedArray() {
+
+        }
+
+        @Test
+        public void pairElements() {
+
+        }
+
+        @Test
+        public void palindromeExample() { // rev of wor is same as given word then its palandrome
+
+        }
+
+        @Test
+        public void rearrangePosNegNumber() {
+
+        }
+
+        @Test
+        public void removeConsecutiveDuplicate() {
+
+        }
+
+        @Test
+        public void revWordInSentence() {
+
+        }
+
+        @Test
+        public void rotatebyN() {
+
+
+        }
+
+        @Test
+        public void secondlargestinArray() {
+
+        }
+
+        @Test
+        public void secondSmallestElementInArray() {
+
+        }
+
+        @Test
+        public void secondSmallestElementInArrayWithoutSorting() {
+
+        }
+
+        @Test
+        public void segreggatearray() {
+
+        }
+
+        @Test
+        public void sumUntilSingleDigit() {
+
+        }
+
+
+
+
+
+}

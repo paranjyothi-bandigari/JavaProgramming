c6f0da372304ec0402f3ba48d91c73b5434b7cfa
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2026-02-08 22:15:28 -0700

    new files added

--- src/test/java/JavaProgramming/Imp/ReverseANumber.java
+++ src/test/java/JavaProgramming/Imp/ReverseANumber.java
@@ -3,12 +3,16 @@ package JavaProgramming.Imp;
 public class ReverseANumber {
     public static void main(String[] args) {
         int num=987654321;
-        int rev=0;
-        while (num != 0) {
-            int digit = num % 10;            // get last digit
-            rev = rev * 10 + digit; // add it to reversed
-            num = num / 10;                   // remove last digit
-        }
+        String numstr=String.valueOf(num);
+        StringBuilder sb=new StringBuilder(numstr);
+        String rev=sb.reverse().toString();
+        System.out.println(rev);
+//        int rev=0;
+//        while (num != 0) {
+//            int digit = num % 10;            // get last digit
+//            rev = rev * 10 + digit; // add it to reversed
+//            num = num / 10;                   // remove last digit
+//        }
 
     }
 }
